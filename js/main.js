189e6c563a0c957bba57610aca5040893e49422e
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2025-11-09 08:19:18 +0100

    1version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-const buttonClick = document.querySelector(click)
+const burgerBtn = document.querySelector('.js-burger');
+const closeBtn = document.querySelector('.js-close');
+const mobileMenu = document.querySelector('.js-mob-menu');
+
+
+function openMenu() {
+    mobileMenu.classList.add('is-open');
+}
+burgerBtn.addEventListener('click', openMenu);
+
+function closeMenu() {
+    mobileMenu.classList.remove('is-open');
+}
+closeBtn.addEventListener('click', closeMenu);
+
+
+const openModalBtn = document.querySelector('#hero-btn');
+const closeModalBtn = document.querySelector('.modal-close');
+const backdrop = document.querySelector('.js-modal');
+
+function openModal() {
+    backdrop.classList.add('is-open');
+    document.body.classList.add('no-scroll');
+}
+
+function closeModal() {
+    backdrop.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
+}
+
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+
+backdrop.addEventListener('click', (e) => {
+    if (e.target === backdrop) closeModal();
+})
+
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape') closeModal();
+})
+
+
